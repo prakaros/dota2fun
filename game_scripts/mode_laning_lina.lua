63c01752ccc3844f1245ec3d9f26359ddb7f6481
prakaros <praks411@gmail.com>
2019-11-25 23:16:04 +0100

    Simple bot script

--- game_scripts/mode_laning_lina.lua
+++ game_scripts/mode_laning_lina.lua
@@ -1,1 +1,36 @@
+local utilities = require("utilities/dota_utilities")
+require("utilities/dota_data_strings")
+require("utilities/state_chart")
+require("utilities/dota_features")
 
+_G._savedEnv = getfenv()
+module( "mode_generic_laning", package.seeall )
+
+local wayPointNav = nil;	
+
+----------------------------------------------------------------------------------------------------
+
+function OnStart()
+	if wayPointNav == nil then
+		wayPointNav = WayPointNavigator:new();
+		wayPointNav:onInit();
+	end
+	wayPointNav:onStart();
+end
+
+----------------------------------------------------------------------------------------------------
+
+function OnEnd()
+	wayPointNav:onEnd();
+end
+
+function Think()
+	if wayPointNav == nil then
+		wayPointNav = WayPointNavigator:new();
+		wayPointNav:onInit();
+		wayPointNav:onStart();
+	end
+	wayPointNav:onThink();
+end
+
+for k,v in pairs( mode_generic_laning ) do	_G._savedEnv[k] = v end
